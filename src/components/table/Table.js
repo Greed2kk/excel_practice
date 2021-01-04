60453690fa3d25f172c04a875f2219c872264ac5
Greed2kk <draiver78@mail.ru>
2021-01-04 23:41:21 +0300

    group select

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,7 @@ import { ExcelComponent } from '@core/ExcelComponent'
 import { resizeHandler } from '@components/table/table.resize'
 import {
   isCell,
+  matrix,
   shouldResize,
 } from '@components/table/table.functions'
 import { createTable } from './table.template'
@@ -35,8 +36,27 @@ export class Table extends ExcelComponent {
     if (shouldResize(e)) {
       resizeHandler(this.$root, e)
     } else if (isCell(e)) {
-      const target = $(e.target)
-      this.selection.select(target)
+      const $target = $(e.target)
+      if (e.shiftKey) {
+        const $cells = matrix(
+          $target,
+          this.selection.current
+        ).map(id => this.$root.find(`[data-id="${id}"]`))
+        this.selection.selectGroup($cells)
+      } else {
+        this.selection.select($target)
+      }
     }
   }
+
+  // onMouseup(e) {
+  //   if (isCell(e)) {
+  //     const $target = $(e.target)
+  //     const $cells = matrix(
+  //       $target,
+  //       this.selection.current
+  //     ).map(id => this.$root.find(`[data-id="${id}"]`))
+  //     this.selection.selectGroup($cells)
+  //   }
+  // }
 }
