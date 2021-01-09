e741295caa8cdccd9c745098ea9d4e324e5e138b
Greed2kk <draiver78@mail.ru>
2021-01-09 06:57:50 +0300

    redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+import { rootReducer } from '@/redux/rootReducer'
 import { Excel } from '@components/excel/Excel'
 import { Formula } from '@components/formula/Formula'
 import { Header } from '@components/header/Header'
 import { Table } from '@components/table/Table'
 import { Toolbar } from '@components/toolbar/Toolbar'
 import './scss/index.scss'
+import { createStore } from '@core/createStore'
+
+const store = createStore(rootReducer)
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 })
 
 excel.render()
