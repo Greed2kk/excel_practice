0566134b9b1625b4ca2ffcac2a4dece5f0e4b268
Greed2kk <draiver78@mail.ru>
2021-04-18 15:56:50 +0300

    feat: few tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@components/(.*)$': '<rootDir>/components/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
